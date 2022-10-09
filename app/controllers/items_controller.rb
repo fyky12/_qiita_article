dfabbef805d762d56ac209d14798acd8a60d4456
fyky12 <fykygtr@gmail.com>
2022-10-10 00:10:17 +0900

    API叩く(アクセストークン設定前)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
   def index
+    #require 'net/http'
+    require 'open-uri'
 
+    access_token = ""
+
+    headers = {'Authorization' => "Bearer #{access_token}"}
+
+    uri = URI.parse("https://qiita.com/api/v2/authenticated_user")
+    res = URI.open(uri, headers)
+
+    @result = JSON.parse(res.read)
+
+    p @result
   end
 end
